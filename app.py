1dbbfdeea15adf81abe158f1e5fcc4cb13ddfd64
Gianni Maggiora <giannimaggiora@Giannis-MacBook-Pro.local>
2021-04-14 02:00:40 +0200

    ad

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import time
 app = Flask(__name__);
 @app.route("/bot", methods=["POST"])
 def response():
-    query = dict(request.form)['query']
-    res = query + " " + time.ctime()
+    ##query = dict(request.form)['query']
+    res = "sdf " + time.ctime()
     return jsonify({"response" : res})
 if __name__=="__main__":
     app.run(host="0.0.0.0",)
